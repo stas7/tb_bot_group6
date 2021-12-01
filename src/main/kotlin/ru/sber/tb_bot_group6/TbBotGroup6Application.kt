4cf6428f0cd7f3baeb926184fdd1930e8992931c
Stanislav Naumenko <Naumenko.s.vik@sberbank.ru>
2021-12-01 22:39:54 +0300

    Initial commit

---
+++ src/main/kotlin/ru/sber/tb_bot_group6/TbBotGroup6Application.kt
@@ -0,0 +1,11 @@
+package ru.sber.tb_bot_group6
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TbBotGroup6Application
+
+fun main(args: Array<String>) {
+	runApplication<TbBotGroup6Application>(*args)
+}
---
+++ src/test/kotlin/ru/sber/tb_bot_group6/TbBotGroup6ApplicationTests.kt
@@ -0,0 +1,13 @@
+package ru.sber.tb_bot_group6
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TbBotGroup6ApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
