0bed6e2f51be80c116f529d52923d9b12fe0647f
Rbster <rbster@yandex.ru>
2021-12-06 11:22:26 +0300

    dateTime wrong format during creation processing correctly

--- src/main/kotlin/ru/sber/tb_bot_group6/finalStateMachine/state/MeetingCreationDateTimeState.kt
+++ src/main/kotlin/ru/sber/tb_bot_group6/finalStateMachine/state/MeetingCreationDateTimeState.kt
@@ -13,6 +13,7 @@ import ru.sber.tb_bot_group6.persistence.repository.CustomerRepository
 import ru.sber.tb_bot_group6.persistence.repository.MeetingRepository
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
+import java.time.format.DateTimeParseException
 import java.time.format.FormatStyle
 import java.util.*
 
@@ -24,33 +25,52 @@ class MeetingCreationDateTimeState : StateInterface{
     lateinit var customerRepository: CustomerRepository
     @Autowired
     lateinit var meetingRepository: MeetingRepository
-    override fun getAnswer(stateInfoDTO: StateInfoDTO): SendMessage {
-        val currentMeeting = requireNotNull(customerRepository.findByTelegramChatId(stateInfoDTO.chatId)
-            ?.currentMeeting)
-        currentMeeting.meetingDate = LocalDateTime.parse(stateInfoDTO.receivedText,
-            DateTimeFormatter.ofPattern("dd.MM.yyyy HH:mm"))
-        meetingRepository.save(currentMeeting)
 
-        val message = SendMessage(stateInfoDTO.chatId.toString(),
-            "Встреча создана!")
-        message.replyMarkup = ReplyKeyboardMarkup
-            .builder()
-            .keyboardRow(
-                KeyboardRow(
-                    listOf(KeyboardButton("/${currentMeeting.id}"))
-                )
+    fun parseOrNull(textToDateTime: String): LocalDateTime? = try {
+            LocalDateTime.parse(
+                textToDateTime,
+                DateTimeFormatter.ofPattern("dd.MM.yyyy HH:mm")
             )
-            .oneTimeKeyboard(true)
-            .build()
+        } catch (e: DateTimeParseException) {
+            null
+        }
 
+
+    override fun getAnswer(stateInfoDTO: StateInfoDTO): SendMessage {
+        val currentMeeting = requireNotNull(customerRepository
+            .findByTelegramChatId(stateInfoDTO.chatId)
+            ?.currentMeeting)
+        val message: SendMessage
+        val dateTime = parseOrNull(stateInfoDTO.receivedText)
+
+        if (dateTime != null) {
+            currentMeeting.meetingDate = dateTime
+            meetingRepository.save(currentMeeting)
+
+            message = SendMessage(stateInfoDTO.chatId.toString(),
+                "Встреча создана!")
+            message.replyMarkup = ReplyKeyboardMarkup
+                .builder()
+                .keyboardRow(
+                    KeyboardRow(
+                        listOf(KeyboardButton("/${currentMeeting.id}"))
+                    )
+                )
+                .oneTimeKeyboard(true)
+                .build()
+        } else {
+            message = SendMessage(stateInfoDTO.chatId.toString(), "Ошибка форматирования, попробуйте снова!")
+        }
         return message
     }
 
     override fun changeState(stateInfoDTO: StateInfoDTO) {
-        val customer = requireNotNull(customerRepository.findByTelegramChatId(stateInfoDTO.chatId))
-        customer.state = MachinesStateEnum.MEETING_DETAILS
-        val resultingCustomer = customerRepository.save(customer)
-        println("------>$resultingCustomer")
+        if (parseOrNull(stateInfoDTO.receivedText) != null) {
+            val customer = requireNotNull(customerRepository.findByTelegramChatId(stateInfoDTO.chatId))
+            customer.state = MachinesStateEnum.MEETING_DETAILS
+            val resultingCustomer = customerRepository.save(customer)
+            println("------>$resultingCustomer")
+        }
     }
 
 }
